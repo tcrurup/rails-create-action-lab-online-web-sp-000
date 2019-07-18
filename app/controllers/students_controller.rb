cedd2174e0912c9501a80839a66b9eded0344e1f
Anthony Rurup <tcrurup@gmail.com>
2019-07-18 00:15:53 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,6 +15,8 @@ class StudentsController < ApplicationController
     @student = Student.new
     @student.first_name = params[:student][:first_name]
     @student.last_name = params[:student][:last_name]
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
